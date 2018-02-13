5e2c69fbda99f13406c5c7a024d5aa9dbc435a0e
liyang <yang.li@zxthealth.com>
2018-02-13 16:41:53 +0800

    update layout

---
+++ config.js
@@ -0,0 +1,19 @@
+/**
+ * 小程序配置文件
+ */
+
+var host = "http://siwhzv.natappfree.cc/api"
+
+var config = {
+
+  // 下面的地址配合云端 Server 工作
+  host,
+
+  // 查询税号列表接口
+  queryTaxUrl: `${host}/queryTax`,
+
+  // 查询详细税号开户行信息接口
+  detailTaxUrl: `${host}/detailTax`
+};
+
+module.exports = config
